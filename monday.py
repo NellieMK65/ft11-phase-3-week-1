28c513bd5699c6751e4a4604aa20395ad9afc0e5
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-11-25 10:01:59 +0300

    intro to python

---
+++ monday.py
@@ -0,0 +1,35 @@
+# this is a single line comment in py
+
+""" -> this is docstring
+we can use this to add multiline comment
+"""
+
+print("Intro to python")
+
+# Datatypes
+
+# 1. str
+print("Jane")
+print('Doe')
+
+# string interpolation using f string
+first_name = "Jane"
+last_name = " Doe"
+print(first_name + last_name)
+print(f"{first_name} Doe".upper())
+print(type(first_name))
+# 2. bool
+
+# 3. int
+
+# 4. float
+
+# 5. list
+
+# 6. tuple
+
+# 7. set
+
+# 8. dict
+
+# 9. None
