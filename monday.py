724305d463951f1ad5bec6edcbe2f6e800a3c3ee
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-11-25 15:45:05 +0300

    data types done

--- monday.py
+++ monday.py
@@ -18,18 +18,50 @@ last_name = " Doe"
 print(first_name + last_name)
 print(f"{first_name} Doe".upper())
 print(type(first_name))
-# 2. bool
+# 2. bool -> True/False
+print(3 > 4)
+# i. Number operators (>,<,>=)
+# ii. Logical operators (and, or, not)
+# iii.
+print(bool(None))
+# 3. int -> whole numbers PEMDAS
+print(type(3))
 
-# 3. int
+# 4. float -> decimal numbers
+print(type(0.2))
 
-# 4. float
+# 5. list -> ordered collection of data are same as arrays in js
+# could map, append, can be indexed
+students = ['Sharon', 'John']
 
-# 5. list
+cars = []
 
-# 6. tuple
+students.append("Brian")
 
-# 7. set
+students[0] = 'Dennis'
+
+print(students)
+
+# 6. tuple -> lists that are immutable
+colors = ('red', 'green', 'blue')
+
+print(type(colors))
+
+# 7. set -> immutable list of elements inside curly braces {}
+# sets also only accept unique values
+unique_students = {'Brian', 'Ian', 'Benjamin', 'Ian'}
+empty_set = set()
+
+print(type(empty_set))
+
+print(unique_students)
 
 # 8. dict
+person = {
+    "first_name": "Jane",
+    "courses": ["Software", "DevOps"]
+}
 
-# 9. None
+print(person['first_name'])
+print(person.get('courses'))
+# 9. None -> null and undefined
